1eefbe4edaf18b295a32963d7b4022cfe92e166b
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-02-21 19:18:59 +0200

    [0.0.3] Add points domain.

---
+++ Source/Engine/Domain/Points/Collection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Liloi\Interstate\Engine\Domain\Ticket;
+
+use Liloi\Tools\Collection as AbstractCollection;
+
+/**
+ * @todo: add tests
+ * @todo: add docs
+ * @package Engine\Domain\Exercise
+ */
+class Collection extends AbstractCollection
+{
+
+}
